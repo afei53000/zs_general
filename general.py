b17045069176ae5ee0fc413e0af06738a0ae6dfc
afei53000 <feitong_liu@yeah.net>
2019-10-10 09:42:53 +0800

    jtf_hs300

--- general.py
+++ general.py
@@ -6,44 +6,79 @@ import numpy as np
 import datetime as dt
 from matplotlib import pyplot as plt
 
-mom=pd.read_csv('berra.csv')
-mom['date']=pd.to_datetime(mom['date'])
-mom.set_index(mom['date'],inplace=True)
-mo=mom['momentum']
-# print(mo)
+
+#数据处理块
+
+
+def cleanData(asset,zb):
+    asset['date']=pd.to_datetime(asset['date'])
+    asset.set_index(asset['date'],inplace=True)
+    asset=asset['asset']
+    # print(mo)
+
+
+    # jtf["date"]=jtf["date"].astype('datetime64')
+    # jtf["jtf"]=jtf["jtf"].astype('float64')
+
+    zb['date']=pd.to_datetime(zb['date'])
+    # jtf.set_index("date", inplace=True)
+
+    zb.set_index(zb['date'],inplace=True)
+    zb['zb_roll']=zb['zb'].rolling(4,min_periods=4,axis=0).mean()
+    zb=zb[['zb','zb_roll']]
+    # print(jtf)
+    # jtf.plot()
+    # plt.show()
+    # jtf.set_index(jtf['date'],inplace=True)
+    # jtf=jtf['jtf']
+# print(jtf)
+    pdatas=pd.concat([zb,asset],axis=1)
+    pd.DataFrame(pdatas).columns=['zb','zb_roll','asset']
+    # print(pdatas)
+    return pdatas
+
+
+#载入数据
+# mom=pd.read_csv('berra.csv')
+# mom=mom.copy()
+# asset=mom[['date','momentum']]
+# pd.DataFrame(asset).columns=['date','asset']
+#
+#
+# jtf=pd.read_csv('jtf.csv')
+# jtf=jtf.copy()
+# pd.DataFrame(jtf).columns=['date','zb']
+#
+# zb=jtf.copy()
+# pdatas=cleanData(asset,zb)
+#数据处理块
+mom=pd.read_csv('hs300.csv')
+mom=mom.copy()
+asset=mom[['date','hs300']]
+pd.DataFrame(asset).columns=['date','asset']
+
 
 jtf=pd.read_csv('jtf.csv')
 jtf=jtf.copy()
-# jtf["date"]=jtf["date"].astype('datetime64')
-# jtf["jtf"]=jtf["jtf"].astype('float64')
+pd.DataFrame(jtf).columns=['date','zb']
 
-jtf['date']=pd.to_datetime(jtf['date'])
-# jtf.set_index("date", inplace=True)
+zb=jtf.copy()
+pdatas=cleanData(asset,zb)
 
-jtf.set_index(jtf['date'],inplace=True)
-jtf['jtf_roll']=jtf['jtf'].rolling(4,min_periods=4,axis=0).mean()
-jtf=jtf[['jtf','jtf_roll']]
-# print(jtf)
-# jtf.plot()
-# plt.show()
-# jtf.set_index(jtf['date'],inplace=True)
-# jtf=jtf['jtf']
-# print(jtf)
 
-pdatas=pd.concat([jtf,mo],axis=1)
-print(pdatas)
 
 for i in range(len(pdatas.index)):
-    if pd.isna(pdatas.momentum[i])==False:
+    if (pd.isna(pdatas.zb[i-1])==False)&(pd.isna(pdatas.zb[i])==True):
         # pdatas.flag[i] = 1
-        pdatas.jtf[i]=pdatas.jtf[i-1]
-        pdatas.jtf_roll[i]=pdatas.jtf_roll[i-1]
-pdatas=pdatas.dropna(subset=['momentum'])
+        pdatas.zb[i]=pdatas.zb[i-1]
+    if (pd.isna(pdatas.zb_roll[i - 1]) == False) & (pd.isna(pdatas.zb_roll[i]) == True):
+        pdatas.zb_roll[i]=pdatas.zb_roll[i-1]
+pdatas=pdatas.dropna(subset=['asset'])
 # print(pdatas)
-pdatas=pdatas[['jtf','momentum','jtf_roll']]
+pdatas=pdatas[['zb','zb_roll','asset']]
+print(pdatas)
 
-
-def Strategy(pdatas, win_long, win_short, lossratio=999):
+def Strategy(pdatas):
     # pdatas = datas.copy();win_long = 12;win_short = 6;lossratio = 999;
     """
     pma：计算均线的价格序列
@@ -67,14 +102,14 @@ def Strategy(pdatas, win_long, win_short, lossratio=999):
         # 仅多择时策略：仅在出现状态 b 的时候做多资产 C，验证得到的结果称为正向关系显著；
         # 当前出现状态b（b状态：宏观指标下降），做多
         # (pd.isna(pdatas.momentum[i])==False)&
-        if (pdatas.jtf[i] < 0):
+        if (pdatas.zb[i] <0):
             # &(pdatas.rolling[i]>0)
             pdatas.flag[i] = 1
             pdatas.position[i + 1] = 1
             # pdatas 是一个Dataframe 有5列 1：时间 2：宏观 3：价格CLOSE 4：position 5:flag
 
             date_in = pdatas.index[i]
-            price_in = pdatas.momentum[i]
+            price_in = pdatas.asset[i]
             pricein.append([date_in, price_in])
         # if (pdatas.jtf[i] > 0):
         #     # &(pdatas.rolling[i]>0)
@@ -113,13 +148,13 @@ def Strategy(pdatas, win_long, win_short, lossratio=999):
     # transactions  是一个有2列的Dataframe  1：时间 2：买入和卖出价格
 
     # pdatas = pdatas.loc[max(0, win_long):, :].reset_index(drop=True)
-    pdatas['ret'] = pdatas.momentum.pct_change(1).fillna(0)
+    pdatas['ret'] = pdatas.asset.pct_change(1).fillna(0)
 
     # print(pdatas.iloc[:,1])#momentum
     # print(pdatas.iloc[:,1].pct_change(1).fillna(0))
     # print(pdatas.position)
     pdatas['nav'] = (1 + pdatas.ret * pdatas.position).cumprod()
-    pdatas['benchmark'] = pdatas.momentum / pdatas.momentum[0]  # 错了
+    pdatas['benchmark'] = pdatas.asset / pdatas.asset[0]  # 错了
     print(pdatas['benchmark'])
     # pdatas 是一个Dataframe 有8列 1：时间 2：宏观 3：价格CLOSE 4：position 5:flag 6.ret 7.nav 8.benchmark
     print(pdatas.ret, pdatas.position)
@@ -150,6 +185,7 @@ def performace(transactions, strategy):
 
     # 年化收益率
     rety = strategy.nav[strategy.shape[0] - 1] ** (N / strategy.shape[0]) - 1
+    bench_rety = strategy.benchmark[strategy.shape[0] - 1] ** (N / strategy.shape[0]) - 1
 
     # 夏普比
     Sharp = (strategy.ret * strategy.position).mean() / (strategy.ret * strategy.position).std() * np.sqrt(N)
@@ -185,7 +221,7 @@ def performace(transactions, strategy):
     # plt.plot(np.arange(strategy.shape[0]), strategy.nav / strategy.benchmark, 'orange', label='RS', linewidth=2)
     # plt.plot(np.arange(strategy.shape[0]), 1 , 'grey', label='1', linewidth=1)
 
-    plt.plot(np.arange(strategy.shape[0]), strategy.jtf / 50000 + 1, 'orange', label='jtf', linewidth=2)
+    plt.plot(np.arange(strategy.shape[0]), strategy.zb / 50000 + 1, 'orange', label='zb', linewidth=2)
 
     # plt.plot(np.arange(strategy.shape[0]), strategy.jtf_roll/50000+1 , 'blue', label='jtf_roll', linewidth=2)
     lim = [1] * 120
@@ -202,9 +238,10 @@ def performace(transactions, strategy):
     print('------------------------------')
     print('夏普比为:', round(Sharp, 2))
     print('年化收益率为:{}%'.format(round(rety * 100, 2)))
-    print('胜率为：{}%'.format(round(VictoryRatio * 100, 2)))
+    print('benchmark年化收益率为:{}%'.format(round(bench_rety * 100, 2)))
+    # print('胜率为：{}%'.format(round(VictoryRatio * 100, 2)))
     print('最大回撤率为：{}%'.format(round(MDD * 100, 2)))
-    print('单次最大亏损为:{}%'.format(round(-maxloss * 100, 2)))
+    # print('单次最大亏损为:{}%'.format(round(-maxloss * 100, 2)))
     print('月均交易次数为：{}(买卖合计)'.format(round(strategy.flag.abs().sum() / strategy.shape[0] * 20, 2)))
 
     result = {'Sharp': Sharp,
@@ -219,4 +256,4 @@ def performace(transactions, strategy):
     return result, result_peryear
 
 
-Strategy(pdatas,12, 4, lossratio=999)
+Strategy(pdatas)
